baebab2576ec3e9742cd90b2970aa6fd83174e11
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-31 08:44:41 +0900

    リポジトリとの接続を追加

--- src/main/java/jp/co/sample/controller/SearchController.java
+++ src/main/java/jp/co/sample/controller/SearchController.java
@@ -1,7 +1,10 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.repository.UserDao;
 
 
 /**
@@ -13,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/search")
 public class SearchController {
+	
+	/** リポジトリとの接続 */
+	@Autowired
+	private UserDao dao;
 
 	
 	/**
