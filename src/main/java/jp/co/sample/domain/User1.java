b0a392a5f0bfd51fddc1b285312fdb3aa2f961f4
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-30 16:48:50 +0900

    Userドメインのidを消して新しくUser1ドメインを作成

--- src/main/java/jp/co/sample/domain/User1.java
+++ src/main/java/jp/co/sample/domain/User1.java
@@ -2,32 +2,39 @@ package jp.co.sample.domain;
 
 
 /**
- * ユーザ情報を表すドメイン.
+ * ユーザー情報を表すドメイン
  * 
- * @author ohyama
+ * @author junpei.oyama
  *
  */
-public class User {
+public class User1 {
 	
-
+	/** id */
+	private Integer id;
 	/** 名前 */
 	private String name;
 	/** 年齢 */
-	private Integer age;
+	private String age;
 	/** 住所 */
 	private String address;
 	
-	//getter,setter
+	/** getter,setter */
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getAge() {
+	public String getAge() {
 		return age;
 	}
-	public void setAge(Integer age) {
+	public void setAge(String age) {
 		this.age = age;
 	}
 	public String getAddress() {
@@ -36,11 +43,5 @@ public class User {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
-	}
-	
 	
 }
