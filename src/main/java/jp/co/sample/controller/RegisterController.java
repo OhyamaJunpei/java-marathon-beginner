7f0aa969c957c7bffd553077e3e3bf015287a1ed
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-30 15:13:01 +0900

    初期画面へのRequestMappingを作成

--- src/main/java/jp/co/sample/controller/RegisterController.java
+++ src/main/java/jp/co/sample/controller/RegisterController.java
@@ -3,8 +3,21 @@ package jp.co.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author ohyama
+ *
+ */
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
+	
+	/**
+	 * 初期画面
+	 * @return　入力画面にフォワード
+	 */
+	@RequestMapping("/index")
+	public String index() {
+		return "buygoods";
+	}
 
 }
