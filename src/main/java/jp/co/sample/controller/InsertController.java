9bfe8272dafaef6fdb899ef22196085e4c1edad8
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-31 12:21:12 +0900

    商品をカートに入れるコントローラを作成

--- src/main/java/jp/co/sample/controller/InsertController.java
+++ src/main/java/jp/co/sample/controller/InsertController.java
@@ -1,27 +1,41 @@
 package jp.co.sample.controller;
 
-import javax.servlet.ServletContext;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Item;
+
+
+/**
+ * 商品をカートに入れるコントローラ.
+ * 
+ * @author junpei.oyama
+ *
+ */
 @Controller
 @RequestMapping("/insert")
 public class InsertController {
 	
-	@Autowired
-	private ServletContext application;
-	
+
 	@Autowired
 	private HttpSession session;
 
 	@RequestMapping("/inCart")
 	public String inCart(int index, String name, Integer price) {
-				
-		session.setAttribute("name", name);
-		session.setAttribute("price", price);
+		
+		Item item = new Item();
+		List<Item> cartList = new LinkedList<>();
+		item.setName(name);
+		item.setPrice(price);
+		cartList.add(item);
+		
+		session.setAttribute("cartList", cartList);
 		
 		return "ViewCartController";
 	}
